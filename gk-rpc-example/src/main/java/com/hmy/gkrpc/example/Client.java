a82a3fb909a2b762b4c794000749cd3a70efa952
hhmy27 <31946303+hhmy27@users.noreply.github.com>
2022-04-02 14:53:47 +0800

    update

--- gk-rpc-example/src/main/java/com/hmy/gkrpc/example/Client.java
+++ gk-rpc-example/src/main/java/com/hmy/gkrpc/example/Client.java
@@ -2,6 +2,9 @@ package com.hmy.gkrpc.example;
 
 import com.hmy.gkrpc.client.RpcClient;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @Description
  */
@@ -13,8 +16,9 @@ public class Client {
         System.out.println(result);
         PrintService printService = client.getProxy(PrintService.class);
         System.out.println(printService.print("i just want to sleep"));
-
 //        System.out.println(result);
 //        System.out.println(service.minus(1, 10));
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
     }
 }
